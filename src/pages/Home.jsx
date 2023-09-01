50ba5386f891d63ece88ed1e79f37f9dcb3afe48
Tynneth Jhoel <tj.fuentes15@gmail.com>
2023-09-01 11:39:29 +0800

    add style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import '../scss/home.scss'
+
 const Home = () => {
 
   return (
@@ -8,34 +11,35 @@ const Home = () => {
             <p>Hi I'm <span>Tynneth</span>, Lorem ipsum dolor sit amet.</p>
           </div>
           <div className="image-container">
-            <img src="..." alt="tj-photo" />
+            <img src="/images/tj.jpg" alt="tj-photo" className="prof-pic" />
           </div>
         </div>
         <div className="languages-container">
           <ul className="all-languages">
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>PHP</li>
-            <li>ReactJs</li>
-            <li>Laravel</li>
+            <li className="language-content nav-link">HTML</li>
+            <li className="language-content nav-link">CSS</li>
+            <li className="language-content nav-link">JavaScript</li>
+            <li className="language-content nav-link">PHP</li>
+            <li className="language-content nav-link">ReactJs</li>
+            <li className="language-content nav-link">Laravel</li>
           </ul>
         </div>
         <div className="projects-container">
-          <div className="first-project">
-            <h3>Mini Project 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, voluptas!</p>
-            <img src="..." alt="project-screenshot" />
+          <div className="card" style={{width: 280}}>
+            <img src="/images/ColorHuePrints.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Color Hue Prints</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <Link to="..." className="btn btn-primary">Go to Website</Link>
+            </div>
           </div>
-          <div className="second-project">
-            <h3>Mini Project 2</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, voluptas!</p>
-            <img src="..." alt="project-screenshot" />
-          </div>
-          <div className="capstone-project">
-            <h3>Capstone Project</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, voluptas!</p>
-            <img src="..." alt="project-screenshot" />
+          <div className="card" style={{width: 280}}>
+            <img src="/images/F2PGaming.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">F2P Gaming</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <Link to="..." className="btn btn-primary">Go to Website</Link>
+            </div>
           </div>
         </div>
       </main>
