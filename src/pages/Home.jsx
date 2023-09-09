1ea13751131358f04bfa837ae3f7ddb15ab821ad
Tynneth Jhoel <tj.fuentes15@gmail.com>
2023-09-09 23:20:22 +0800

    In progress skills content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
 
+
 const Home = () => {
 
   return (
     <>
       <main className="main-container">
-        <div className="intro-container">
-          <h4>Hello! I'm <span>Tynneth</span>, a Full Stack Web Developer</h4>
-          <p>With expertise in both fron-end and back-end technologies.
-            I  bring websites and web applications to life, from pixel-perfect
-            user interfaces to building robust server-side systems.
-          </p>
+        <div className="profile-container">
+          <div className="intro-container">
+            <h4>Hello! I'm <span>Tynneth</span>, a Full Stack Web Developer</h4>
+            <p>With expertise in both fron-end and back-end technologies.
+              I  bring websites and web applications to life, from pixel-perfect
+              user interfaces to building robust server-side systems.
+            </p>
+          </div>
+
+          <div className="image-container">
+            <img src="../images/tj.png" alt="tj-photo" />
+          </div>
         </div>
+
+        <div className="prog-container">
+          <h3>Skills</h3>
+        </div>
+        
       </main>
     </>
   )
