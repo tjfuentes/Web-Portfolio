f33e0dbbac7d97131ee1e0a92db5bdee668cb5e7
Tynneth Jhoel <tj.fuentes15@gmail.com>
2023-09-09 22:36:50 +0800

    reset the code and start a new portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
 import Home from './pages/Home'
-import Footer from './components/Footer'
-import AboutMe from './pages/AboutMe'
-import Projects from './pages/Projects'
+import './scss/app.scss'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="/About" element={<AboutMe />}/>
-          <Route path="/Projects" element={<Projects />} />
         </Routes>
-        <Footer />
       </BrowserRouter>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,16 @@
 import { Link } from "react-router-dom";
-import '../scss/home.scss'
 
 const Home = () => {
 
   return (
     <>
       <main className="main-container">
-        <div className="profile-container">
-          <div className="profile-summary">
-            <p>Hi I'm <span>Tynneth</span>, Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="image-container">
-            <img src="/images/tj.jpg" alt="tj-photo" className="prof-pic" />
-          </div>
-        </div>
-        <div className="languages-container">
-          <ul className="all-languages">
-            <li className="language-content nav-link">HTML</li>
-            <li className="language-content nav-link">CSS</li>
-            <li className="language-content nav-link">JavaScript</li>
-            <li className="language-content nav-link">PHP</li>
-            <li className="language-content nav-link">ReactJs</li>
-            <li className="language-content nav-link">Laravel</li>
-          </ul>
-        </div>
-        <div className="projects-container">
-          <div className="card" style={{width: 280}}>
-            <img src="/images/ColorHuePrints.png" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Color Hue Prints</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <Link to="..." className="btn btn-primary">Go to Website</Link>
-            </div>
-          </div>
-          <div className="card" style={{width: 280}}>
-            <img src="/images/F2PGaming.png" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">F2P Gaming</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <Link to="..." className="btn btn-primary">Go to Website</Link>
-            </div>
-          </div>
+        <div className="intro-container">
+          <h4>Hello! I'm <span>Tynneth</span>, a Full Stack Web Developer</h4>
+          <p>With expertise in both fron-end and back-end technologies.
+            I  bring websites and web applications to life, from pixel-perfect
+            user interfaces to building robust server-side systems.
+          </p>
         </div>
       </main>
     </>
