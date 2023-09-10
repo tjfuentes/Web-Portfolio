b29ff92d04974adff355434af7df8936ec3d527f
Tynneth Jhoel <tj.fuentes15@gmail.com>
2023-09-10 23:04:25 +0800

    add framework content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="profile-container">
           <div className="intro-container">
             <h4>Hello! I'm <span>Tynneth</span>, a Full Stack Web Developer</h4>
-            <p>With expertise in both fron-end and back-end technologies.
+            <p>With expertise in both front-end and back-end technologies.
               I  bring websites and web applications to life, from pixel-perfect
               user interfaces to building robust server-side systems.
             </p>
@@ -17,31 +17,57 @@ const Home = () => {
 
           <div className="image-container">
             <img src="../images/tj.png" alt="tj-photo" />
+            <div className="socmed">              
+              <Link to="https://github.com/tjfuentes"><i className="fa-brands fa-github"></i></Link>
+              <Link to="https://www.linkedin.com/in/tynneth-fuentes-990692233/"><i className="fa-brands fa-linkedin"></i></Link>
+              <Link to="https://www.facebook.com/tynneth.jhoel/"><i className="fa-brands fa-facebook"></i></Link>
+            </div>
           </div>
         </div>
 
-        <div className="prog-container">
-          <h3>Front-End</h3>
+        <div className="front-container">
+          <h3>Programming Languages</h3>
 
           <div className="technology">
             <div className="tools">
-              <i class="fa-brands fa-html5"></i>
+              <i className="fa-brands fa-html5"></i>
               <h4>HTML</h4>
             </div>
             <div className="tools">
-              <i class="fa-brands fa-css3"></i>
+              <i className="fa-brands fa-css3"></i>
               <h4>CSS</h4>
             </div>
             <div className="tools">
-              <i class="fa-brands fa-square-js"></i>
+              <i className="fa-brands fa-square-js"></i>
               <h4>JavaScript</h4>
             </div>
             <div className="tools">
-              <i class="fa-brands fa-php"></i>
-              <h4>PHP</h4>
+              <i className="fa-brands fa-php"></i>
+              <h4>JavaScript</h4>
             </div>
           </div>
         </div>
+
+        <div className="back-container">
+          <h3>Framework</h3>
+
+          <div className="technology">
+            <div className="tools">
+              <i class="fa-brands fa-react"></i>
+              <h4>React</h4>
+            </div>
+            <div className="tools">
+              <i class="fa-brands fa-bootstrap"></i>
+              <h4>Bootstrap</h4>
+            </div>
+            <div className="tools">
+              <i class="fa-brands fa-laravel"></i>
+              <h4>Laravel</h4>
+            </div>
+          </div>
+        </div>
+
+        
         
       </main>
     </>
