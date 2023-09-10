499f74e050c7a221b600446a424c99021a55263d
Tynneth Jhoel <tj.fuentes15@gmail.com>
2023-09-10 16:53:42 +0800

    added Front-end information

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,26 @@ const Home = () => {
         </div>
 
         <div className="prog-container">
-          <h3>Skills</h3>
+          <h3>Front-End</h3>
+
+          <div className="technology">
+            <div className="tools">
+              <i class="fa-brands fa-html5"></i>
+              <h4>HTML</h4>
+            </div>
+            <div className="tools">
+              <i class="fa-brands fa-css3"></i>
+              <h4>CSS</h4>
+            </div>
+            <div className="tools">
+              <i class="fa-brands fa-square-js"></i>
+              <h4>JavaScript</h4>
+            </div>
+            <div className="tools">
+              <i class="fa-brands fa-php"></i>
+              <h4>PHP</h4>
+            </div>
+          </div>
         </div>
         
       </main>
