5544c453ddebee80ff4979819a67499c30af2d91
Tynneth Jhoel <tj.fuentes15@gmail.com>
2023-09-27 12:50:34 +0800

    revise portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,87 +6,119 @@ const Home = () => {
   return (
     <>
       <main className="main-container">
-        <div className="profile-container">
-          <div className="intro-container">
-            <h4>Hello! I'm <span>Tynneth</span>, a Full Stack Web Developer</h4>
-            <p>With expertise in both front-end and back-end technologies.
-              I  bring websites and web applications to life, from pixel-perfect
-              user interfaces to building robust server-side systems.
-            </p>
-          </div>
-
-          <div className="image-container">
-            <img src="../images/tj.png" alt="tj-photo" />
-            <div className="socmed">              
-              <Link to="https://github.com/tjfuentes"><i className="fa-brands fa-github"></i></Link>
-              <Link to="https://www.linkedin.com/in/tynneth-fuentes-990692233/"><i className="fa-brands fa-linkedin"></i></Link>
-              <Link to="https://www.facebook.com/tynneth.jhoel/"><i className="fa-brands fa-facebook"></i></Link>
+        <section className="glass">
+          <div className="dashboard">
+            <div className="user">
+              <img src="/images/tj.png" alt="..." />
+              <h3>Tynneth</h3>
+              <p>Pro member</p>
             </div>
-          </div>
-        </div>
-
-        <div className="front-container">
-          <h3>Programming Languages</h3>
-
-          <div className="technology">
-            <div className="tools">
-              <Link to="https://web.dev/learn/html/"><i className="fa-brands fa-html5"></i></Link>
-              <h4>HTML</h4>
-            </div>
-            <div className="tools">
-            <Link to="https://web.dev/learn/css/"><i className="fa-brands fa-css3"></i></Link>
-              <h4>CSS</h4>
-            </div>
-            <div className="tools">
-            <Link to="https://www.javascript.com/learn/strings"><i className="fa-brands fa-square-js"></i></Link>
-              <h4>JavaScript</h4>
-            </div>
-            <div className="tools">
-            <Link to="https://www.php.net/"><i className="fa-brands fa-php"></i></Link>
-              <h4>PHP</h4>
-            </div>
-          </div>
-        </div>
-
-        <div className="framework-container">
-          <div className="first-section">
-            <h3>Framework</h3>
-
-            <div className="technology">
-              <div className="tools">
-              <Link to="https://react.dev/learn"><i class="fa-brands fa-react"></i></Link>
-                <h4>React</h4>
+            <div className="links">
+              <div className="link">
+                <i class='bx bx-user'></i>
+                <h2>About</h2>
               </div>
-              <div className="tools">
-              <Link to="https://getbootstrap.com/"><i class="fa-brands fa-bootstrap"></i></Link>
-                <h4>Bootstrap</h4>
+              <div className="link">
+                <i class='bx bx-task' ></i>
+                <h2>Projects</h2>
               </div>
-              <div className="tools">
-              <Link to="https://laravel.com/docs/10.x/readme"><i class="fa-brands fa-laravel"></i></Link>
-                <h4>Laravel</h4>
+              <div className="link">
+                <i class='bx bxl-react' ></i>
+                <h2>Skills</h2>
+              </div>
+              <div className="link">
+                <i class='bx bxl-meta' ></i>
+                <h2>Socmed</h2>
               </div>
             </div>
+            <div className="pro">
+              <h2>Hire Me</h2>
+              <img src="/images/tj.png" alt="..." />
+            </div>
           </div>
-
-          <div className="second-section">
-            <h3>Database</h3>
-
-            <div className="technology">
-              <div className="tools">
-              <Link to="https://dev.mysql.com/doc/"><i class="fa-solid fa-database"></i></Link>
-                <h4>MySQL</h4>
+          <div className="games">
+            <div className="status">
+              <h1>Languages & FrameWorks</h1>
+              <div className="cards">
+                <div className="kard">
+                  <i class='bx bxl-html5' ></i>
+                  <div className="card-info">
+                    <h2>HTML</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <div className="progressbar"></div>
+                  </div>
+                  <h2 className="percentage">60%</h2>
+                </div>
+                <div className="kard">
+                  <i class='bx bxl-css3' ></i>
+                  <div className="card-info">
+                    <h2>CSS</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <div className="progressbar"></div>
+                  </div>
+                  <h2 className="percentage">60%</h2>
+                </div>
+                <div className="kard">
+                  <i class='bx bxl-javascript' ></i>
+                  <div className="card-info">
+                    <h2>JAVASCRIPT</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <div className="progressbar"></div>
+                  </div>
+                  <h2 className="percentage">60%</h2>
+                </div>
+                <div className="kard">
+                  <i class='bx bxl-php' ></i>
+                  <div className="card-info">
+                    <h2>PHP</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <div className="progressbar"></div>
+                  </div>
+                  <h2 className="percentage">60%</h2>
+                </div>
+                <div className="kard">
+                  <i class='bx bxs-data' ></i>
+                  <div className="card-info">
+                    <h2>MySQL</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <div className="progressbar"></div>
+                  </div>
+                  <h2 className="percentage">60%</h2>
+                </div>
+                <div className="kard">
+                  <i class='bx bxl-react' ></i>
+                  <div className="card-info">
+                    <h2>ReactJs</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <div className="progressbar"></div>
+                  </div>
+                  <h2 className="percentage">60%</h2>
+                </div>
+                <div className="kard">
+                  <i class='bx bxl-bootstrap' ></i>
+                  <div className="card-info">
+                    <h2>BootStrap</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <div className="progressbar"></div>
+                  </div>
+                  <h2 className="percentage">60%</h2>
+                </div>
+                <div className="kard">
+                  <i class='bx bx-data' ></i>
+                  <div className="card-info">
+                    <h2>Laravel</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <div className="progressbar"></div>
+                  </div>
+                  <h2 className="percentage">60%</h2>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="about-me">
-          
-        </div>
-
-        
-        
+        </section>
       </main>
+      <div className="circle1"></div>
+      <div className="circle2"></div>
     </>
   )
 }
